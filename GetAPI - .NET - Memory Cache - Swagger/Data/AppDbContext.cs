42dd4e2cbf6d7c0c3c8efb7ded54532d49652a0e
EmreSpo <emre.salihpasaogluTR@gmail.com>
2024-08-23 16:30:08 +0300

    Add files via upload

--- GetAPI - .NET - Memory Cache - Swagger/Data/AppDbContext.cs
+++ GetAPI - .NET - Memory Cache - Swagger/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using YourNamespace.Models; // Address, Company, Geo ve User sınıflarının burada tanımlandığından emin olun
 
 namespace YourNamespace.Data
 {
@@ -8,7 +9,7 @@ namespace YourNamespace.Data
     public class AppDbContext : DbContext
     {
         /// <summary>
-        /// AppDbContext sınıfının bir örneğini oluştur
+        /// AppDbContext sınıfının bir örneğini oluşturur
         /// </summary>
         /// <param name="options">Veritabanı bağlamı seçenekleri</param>
         public AppDbContext(DbContextOptions<AppDbContext> options)
@@ -37,19 +38,49 @@ namespace YourNamespace.Data
         public DbSet<Geo> Geos { get; set; }
 
         /// <summary>
-        /// Model yapılandırmasını tanımla
+        /// Model yapılandırmasını tanımlar
         /// </summary>
         /// <param name="modelBuilder">Model yapılandırıcı</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            // User konfigürasyonu
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.HasOne(e => e.Address)
+                      .WithMany(a => a.Users)
+                      .HasForeignKey(e => e.AddressId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasOne(e => e.Company)
+                      .WithMany(c => c.Users)
+                      .HasForeignKey(e => e.CompanyId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            // Address konfigürasyonu
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Street).IsRequired();
+                entity.Property(e => e.City).IsRequired();
+                entity.Property(e => e.Zipcode).IsRequired();
+
+                // Mapping for Geo
+                entity.HasOne(a => a.Geo)
+                      .WithOne(g => g.Address)
+                      .HasForeignKey<Geo>(g => g.AddressId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
             // Company konfigürasyonu
             modelBuilder.Entity<Company>(entity =>
             {
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Name).IsRequired();
-                // Diğer konfigürasyonlar ekle
             });
 
             // Geo konfigürasyonu
@@ -58,30 +89,6 @@ namespace YourNamespace.Data
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Lat).IsRequired();
                 entity.Property(e => e.Lng).IsRequired();
-                // Diğer konfigürasyonlar eklenebilir
-            });
-
-            // Address konfigürasyonu
-            modelBuilder.Entity<Address>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.HasOne(e => e.Geo)
-                      .WithOne()
-                      .HasForeignKey<Address>(e => e.Id);
-                // Diğer konfigürasyonlar eklenebilir
-            });
-
-            // User konfigürasyonu
-            modelBuilder.Entity<User>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.HasOne(e => e.Address)
-                      .WithOne()
-                      .HasForeignKey<User>(e => e.Id);
-                entity.HasOne(e => e.Company)
-                      .WithOne()
-                      .HasForeignKey<User>(e => e.Id);
-                // Diğer konfigürasyonlar eklenebilir
             });
         }
     }
